ff0ecc9304e545924489ffcd001fc9464f5e635b
Akirakone <ca646@khels.org>
2021-10-17 21:23:19 -0400

    fizzbuzz

---
+++ index.js
@@ -0,0 +1,21 @@
+
+for (var i=1; i<=100; i++)
+{ 
+    var fizzbuzz=( (i%3==0) && (i%5==0))
+    var fizz= i % 3 == 0;
+    var buzz = i % 5 == 0;
+    
+    if(fizzbuzz==true){
+console.log("fizzbuzz")
+ }
+ else if(fizz ==true) {
+    console.log("fizz");
+ }
+else if(buzz == true) {
+        console.log("buzz");
+ }
+
+ else {
+ console.log(i)
+}
+}
